64db76a5980cf9879ab4bc4947d1e6867d48dd9e
Ciao Huang <riverciao@gmail.com>
2018-05-28 15:49:22 +0800

    add readSpot to Router

--- TaipeiPark/API/Router.swift
+++ TaipeiPark/API/Router.swift
@@ -12,10 +12,12 @@ enum Router {
     
     case readParks(forAmount: Int, fromLastReadIndex: Int?)
     case readFacility(byParkName: String)
+    case readSpot(byParkName: String)
     
     static let baseURL = URL(string: "https://beta.data.taipei/opendata/datalist/apiAccess")!
     
     var queryItems: [URLQueryItem] {
+        
         switch self {
         case .readParks(let limit, let offset):
             var queryParameters: [String : String] = [
@@ -28,14 +30,23 @@ enum Router {
             }
             queryParameters["offset"] =  String(offset)
             return queryParameters.map { URLQueryItem(name: $0, value: $1)}
+            
         case .readFacility(let parkName):
-                let queryParameters: [String : String] = [
+            let queryParameters: [String : String] = [
+                "scope": "resourceAquire",
+                "rid": "97d0cf5c-dc1f-4b5e-8d02-a07e7cc82db7",
+                "q": parkName
+            ]
+            return queryParameters.map { URLQueryItem(name: $0, value: $1)}
+       
+        case .readSpot(let parkName):
+            let queryParameters: [String : String] = [
                 "scope": "resourceAquire",
                 "rid": "bf073841-c734-49bf-a97f-3757a6013812",
                 "q": parkName
             ]
             return queryParameters.map { URLQueryItem(name: $0, value: $1)}
-        }
+         }
     }
     var httpMethod: HTTPMethod {
         return .get
