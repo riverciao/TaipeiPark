25caddd3552f9a76208ab02896e086b295e93c6d
Ciao Huang <riverciao@gmail.com>
2018-06-13 11:56:07 +0800

    add background queue in cache

--- TaipeiPark/Library/Cache/MemoryCache.swift
+++ TaipeiPark/Library/Cache/MemoryCache.swift
@@ -18,16 +18,20 @@ class MemoryCache: Cache {
     }
     
     func store<T: Cachable>(object: T, for key: URL, completion: Cache.Completion?) {
-        let key = key as AnyObject
-        let object = object as AnyObject
-        cache.setObject(object, forKey: key)
-        completion?()
+        DispatchQueue.global().async {
+            let key = key as AnyObject
+            let object = object as AnyObject
+            self.cache.setObject(object, forKey: key)
+            completion?()
+        }
     }
     
     func retrieve<T: Cachable>(for key: URL, completion: @escaping (T) -> Void) {
-        let key = key as AnyObject
-        if let object = cache.object(forKey: key) as? T {
-            completion(object)
+        DispatchQueue.global().async {
+            let key = key as AnyObject
+            if let object = self.cache.object(forKey: key) as? T {
+                completion(object)
+            }
         }
     }
 }
