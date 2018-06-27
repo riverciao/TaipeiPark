c20f0a51b3a3cf6439d95efa7901450f3d15017a
Ciao Huang <riverciao@gmail.com>
2018-06-27 14:36:32 +0800

    modify cache request

--- TaipeiPark/Library/Cache/ImageCacher.swift
+++ TaipeiPark/Library/Cache/ImageCacher.swift
@@ -13,17 +13,28 @@ struct ImageCacher {
     static private let cache = NSCache<AnyObject, AnyObject>()
     
     static func loadImage(with url: URL, into imageView: UIImageView) {
+        
+        // if already have image in cache, set image into imageView
         if let cachedImage = ImageCacher.cache.object(forKey: url as AnyObject) as? UIImage {
             imageView.image = cachedImage
         } else {
+            
+            // if do not have image in cache, make image request and save it in cache
             DispatchQueue.global().async {
-                guard let imageData = try? Data(contentsOf: url) else { return }
-                guard let image = UIImage(data: imageData) else { return }
-                ImageCacher.cache.setObject(image as AnyObject, forKey: url as AnyObject)
-                
-                DispatchQueue.main.async {
-                    imageView.image = image
-                }
+                let request = URLRequest(url: url)
+                let session = URLSession.shared
+                request.responseData(urlSession: session, { (response) in
+                    switch response.result {
+                    case .success(let imageData):
+                        guard let image = UIImage(data: imageData) else { return }
+                        ImageCacher.cache.setObject(image as AnyObject, forKey: url as AnyObject)
+                        DispatchQueue.main.async {
+                            imageView.image = image
+                        }
+                    case .failure:
+                        break
+                    }
+                })
             }
         }
     }
